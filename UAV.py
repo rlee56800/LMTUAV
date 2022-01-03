21af0ede90bb65bfdb83393d6eb33e10d659c6cc
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-01-03 15:07:22 -0800

    aesthetics

--- UAV.py
+++ UAV.py
@@ -1,27 +1,25 @@
 import matplotlib.pyplot as plt
 
+########## CHANGE FILE NAME HERE ########## 
+name = 'Log Outputs 2021_12_24/log_output_2021_12_24 (1).txt'
+
 longitude = []
 lattitude = []
 
-map = {}
-
-file = open(r"C:\\Users\\justi\Desktop\\logData.txt", "r+")
-
 index = 0
 
-for line in file:
-    index += 1
+with open(name) as file:
+    for line in file:
+        index += 1
 
-    if 'Current lattitude' in line:
-        i = line.split()
-        i = i[6]
-        lattitude.append(i)
-    elif 'Current longitude' in line:
-        i = line.split()
-        i = i[6]
-        longitude.append(i)
+        if 'Current lattitude' in line:
+            i = line.split()
+            i = i[6]
+            lattitude.append(i)
+        elif 'Current longitude' in line:
+            i = line.split()
+            i = i[6]
+            longitude.append(i)
 
 plt.plot(lattitude, longitude)
 plt.show()
-
-file.close()
