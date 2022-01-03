f7f2dee81766ace9e4e71849c0973c7c2f0e8170
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-01-03 15:55:11 -0800

    IT WORKSSSSSSSSSSSSSSS

--- UAV.py
+++ UAV.py
@@ -2,24 +2,28 @@ import matplotlib.pyplot as plt
 
 ########## CHANGE FILE NAME HERE ########## 
 name = 'Log Outputs 2021_12_24/log_output_2021_12_24 (1).txt'
+title = 'Flight Graph: 12/24/21 Simulation 1'
 
 longitude = []
 lattitude = []
 
-index = 0
-
 with open(name) as file:
     for line in file:
-        index += 1
 
         if 'Current lattitude' in line:
             i = line.split()
             i = i[6]
-            lattitude.append(i)
+            lattitude.append(float(i))
         elif 'Current longitude' in line:
             i = line.split()
             i = i[6]
-            longitude.append(i)
+            longitude.append(float(i))
 
+plt.scatter(lattitude, longitude)
 plt.plot(lattitude, longitude)
+
+plt.title(title)
+plt.xlabel('Latitude')
+plt.ylabel('Longitude')
+
 plt.show()
