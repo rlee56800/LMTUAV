e98b68c80dd2bef936a0c067817cc260b73e8596
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-07-12 14:39:11 -0700

    7/07 runs

--- Python GUI/REU_graph_july7.py
+++ Python GUI/REU_graph_july7.py
@@ -9,89 +9,83 @@ import matplotlib.pyplot as plt
 ########## CHANGE FILE NAME HERE ##########
 # This is placeholder data/allows program to be run without GUI
 title_of_graph = 'Flight Graph: 7/07 Flight Test'
-name_of_file = 'Python GUI/Log Outputs/flightTest_log_output_2022_07_08.1.txt'
+name_of_file = 'Python GUI/Log Outputs/flightTest_log_output_2022_07_07.txt'
 #name_of_file = 'Python GUI/Log Outputs/flightTest_log_output_2022_07_07.txt'
 ## NOTE: check if file has completed time stamps (i.e. has both future x AND y pos)
-show_predicted = [] # index/indices of value to show predicted values
-show_intruder = 1
 
 def splitter(input_str: str, isX: bool):
+    # takes floats out of given String
+    # i = ["text", "position", "(x,y):", "(-1234,", "5678)"]
+    #                                  x         y
+
     i = input_str.split()
     
-    # i = ["text", "text", "text", "(-1234,", "5678)"]
-    #                                  x         y
-    if isX:
-        return float(i[2][:-1])
-    else:
-        return float(i[3])
+    if isX: # procedure for X slightly different
+        return float(i[3][1:-1]) / 139
+    else: # is y
+        return float(i[4][:-1]) / 111
 
-def main(graph_name: str, file_name: str, map_intruder: int, predicted_indices = [], plotted_point = [], predicted_collision_point = []):
+def main(graph_name: str, file_name: str):
     ########## Graphing ##########
-    # waypoint = []
-    # longitude = []
-    # lattitude = []
-    # future_pos_x = []
-    # future_pos_y = []
-    # x = []
-    # # y = []
-
-    # intruder_longitude = []
-    # intruder_lattitude = []
-
-    # waypoint_latitude = []
-    # waypoint_longitude = []
-
-    own_x = []
-    own_y = []
-    intr_x = []
-    intr_y = []
+    own_x = [] # lon of ownship
+    own_y = [] # lat of ownship
+    intr_x = [] # lon of intruder
+    intr_y = [] # lat of intruder
+    avoid_x = [] # temp storage for lon of points in 1 avoid maneuver
+    avoid_y = [] # temp storage for lat of points in 1 avoid maneuver
+    all_avoid_x = [] # collection of avoid_x values (array of arrays)
+    all_avoid_y = [] # collection of avoid_y values (array of arrays)
+    save_next = False
 
     with open(file_name) as file:
         for line in file:
-            if 'own position:' in line:
-                print("own found")
+            if 'own position' in line:
                 own_x.append(splitter(line, True))
                 own_y.append(splitter(line, False))
-            elif 'intr position:' in line:
-                print("intruder found")
+                if save_next: # after avoid() is called
+                    avoid_x.append(own_x[-1])
+                    avoid_y.append(own_y[-1])
+                    save_next = False
+            elif 'intr position' in line:
                 intr_x.append(splitter(line, True))
                 intr_y.append(splitter(line, False))
+            if 'avoid' in line:
+                save_next = True
+
+                if avoid_x and (avoid_x[-1] != own_x[-1] and avoid_y[-1] != own_y[-1]):
+                    # if the last avoid_x value and own_x value aren't the same,
+                    # then the plane finished avoiding
+                    # (i.e. start a new array)
+                    all_avoid_x.append(avoid_x)
+                    all_avoid_y.append(avoid_y)
+                    avoid_x = []
+                    avoid_y = []
+
+                avoid_x.append(own_x[-1])
+                avoid_y.append(own_y[-1])
+
+    if avoid_x:
+        all_avoid_x.append(avoid_x)
+        all_avoid_y.append(avoid_y)
 
     plt.figure(figsize=(10, 7)) # Window size
 
 
     # for current vehicle
     #plt.scatter(lattitude[1:], longitude[1:], color='black') # Creates scatter plot (dots)
-    plt.plot(own_x, own_y, color='black', zorder=1) # Creates line
-    print(intr_x)
+    plt.plot(own_x, own_y, color='black', zorder=1) # Creates line for ownship
 
-    # for intruder vehicle
-    if True:
-        #print(len(intruder_lattitude), len(intruder_longitude))
-        #plt.scatter(intruder_lattitude, intruder_longitude, color='red') # Creates scatter plot (dots)
-        plt.plot(intr_x, intr_y, color='red', zorder=1) # Creates line
-        # throw error if either are empty
+    plt.plot(intr_x, intr_y, color='orange', zorder=1) # Creates line for intruder
 
+    for i in range(len(all_avoid_x)):
+        plt.plot(all_avoid_x[i], all_avoid_y[i], color = 'blue', zorder = 1) # creates line for each avoid maneuver
+    
+    plt.plot(own_x[0], own_y[0], color = 'green', marker = 'X', markersize = '10', zorder = 1) # Creates starting point for ownship
+    plt.plot(own_x[-1], own_y[-1], color = 'red', marker = 'X', markersize = '10', zorder = 1) # Creates ending point for ownship
 
-    # TESTING SPACE
-    # plt.scatter(-117.whatever, 34.whatever, color = 'green')
-    # plt.scatter(-117.793221, 34.045700, color = 'blue')
-    #plt.scatter(intr_x[1], intr_y[1], color = 'red')
-    # plt.scatter(-117.812176, 34.044746, color = 'orange')
-    # plt.scatter(-117.817139, 34.044439, color = 'green')
-    # plt.scatter(-117.811862, 34.038192, color = 'green')
-    # plt.scatter(-117.781062, 34.089111, color = 'purple')
-    '''
-
-2022-04-28 16:20:48.593403 : Current lattitude : 
-2022-04-28 16:20:48.593403 : Current longitude : 
-2022-04-28 16:20:48.593403 : Intruder X Velocity : -4.14
-2022-04-28 16:20:48.593403 : Intruder Y Velocity : -21.76
-2022-04-28 16:20:48.593403 : Intruder lattitude : 34.0389766
-2022-04-28 16:20:48.593403 : Intruder longitude : -117.80709119999999
-2022-04-28 16:20:48.593403: Predicted crash lattitude : 34.038433512448414
-2022-04-28 16:20:48.593403: Predicted crash longitude : -117.80937068414933
-2022-04-28 16:20:48.593403: Crash location :  '''
+    
+    plt.plot(intr_x[0], intr_y[0], color = 'green', marker = 'X', markersize = '10', zorder = 1) # Creates starting point for intruder
+    plt.plot(intr_x[-1], intr_y[-1], color = 'red', marker = 'X', markersize = '10', zorder = 1) # Creates ending point for intruder
 
     plt.ticklabel_format(useOffset=False) # Display axes correctly
 
@@ -102,4 +96,4 @@ def main(graph_name: str, file_name: str, map_intruder: int, predicted_indices =
     plt.show()
 
 if __name__ == '__main__':
-    main(title_of_graph, name_of_file, show_intruder, show_predicted)
+    main(title_of_graph, name_of_file)
