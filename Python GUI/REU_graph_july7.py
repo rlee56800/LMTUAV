800df1e951db298c2ad6ca2ff1c6ccf7b6f53ca7
LuciaPiner73 <lpiner@poets.whittier.edu>
2022-07-13 10:06:23 -0700

    name of file "../"

--- Python GUI/REU_graph_july7.py
+++ Python GUI/REU_graph_july7.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 ########## CHANGE FILE NAME HERE ##########
 # This is placeholder data/allows program to be run without GUI
 title_of_graph = 'Flight Graph: 7/07 Flight Test'
-name_of_file = 'Python GUI/Log Outputs/flightTest_log_output_2022_07_07.txt'
+name_of_file = '../Python GUI/Log Outputs/flightTest_log_output_2022_07_07.txt'
 #name_of_file = 'Python GUI/Log Outputs/flightTest_log_output_2022_07_07.txt'
 ## NOTE: check if file has completed time stamps (i.e. has both future x AND y pos)
 
